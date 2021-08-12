c6f2d8b247d37d9c821651665cba0a0450fa7b4c
Michail Pukhov <mik@inmircom.ru>
2021-08-12 13:57:58 +0300

    123

---
+++ .terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "3.53.0"
+  constraints = "~> 3.0"
+  hashes = [
+    "h1:w/1Yp1xSRLy5vOodhc/Wy6zrZM5T+2pXCOfYu5H/FFc=",
+    "zh:35a77c79170b0cf3fb7eb835f3ce0b715aeeceda0a259e96e49fed5a30cf6646",
+    "zh:519d5470a932b1ec9a0fe08876c5e0f0f84f8e506b652c051e4ab708be081e89",
+    "zh:58cfa5b454602d57c47acd15c2ad166a012574742cdbcf950787ce79b6510218",
+    "zh:5fc3c0162335a730701c0175809250233f45f1021da8fa52c73635e4c08372d8",
+    "zh:6790f9d6261eb4bd5cdd7cd9125f103befce2ba127f9ba46eef83585b86e1d11",
+    "zh:76e1776c3bf9568d520f78419ec143c081f653b8df4fb22577a8c4a35d3315f9",
+    "zh:ca8ed88d0385e45c35223ace59b1bf77d81cd2154d5416e63a3dddaf0def30e6",
+    "zh:d002562c4a89a9f1f6cd8d854fad3c66839626fc260e5dde5267f6d34dbd97a4",
+    "zh:da5e47fb769e90a2f16c90fd0ba95d62da3d76eb006823664a5c6e96188731b0",
+    "zh:dfe7f33ec252ea550e090975a5f10940c27302bebb5559957957937b069646ea",
+    "zh:fa91574605ddce726e8a4e421297009a9dabe023106e139ac46da49c8285f2fe",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version = "3.1.0"
+  hashes = [
+    "h1:ekOxs6MjdIElt8h9crEVaOwWbEqtfUUfArtA13Jkk6A=",
+    "zh:3d46616b41fea215566f4a957b6d3a1aa43f1f75c26776d72a98bdba79439db6",
+    "zh:623a203817a6dafa86f1b4141b645159e07ec418c82fe40acd4d2a27543cbaa2",
+    "zh:668217e78b210a6572e7b0ecb4134a6781cc4d738f4f5d09eb756085b082592e",
+    "zh:95354df03710691773c8f50a32e31fca25f124b7f3d6078265fdf3c4e1384dca",
+    "zh:9f97ab190380430d57392303e3f36f4f7835c74ea83276baa98d6b9a997c3698",
+    "zh:a16f0bab665f8d933e95ca055b9c8d5707f1a0dd8c8ecca6c13091f40dc1e99d",
+    "zh:be274d5008c24dc0d6540c19e22dbb31ee6bfdd0b2cddd4d97f3cd8a8d657841",
+    "zh:d5faa9dce0a5fc9d26b2463cea5be35f8586ab75030e7fa4d4920cd73ee26989",
+    "zh:e9b672210b7fb410780e7b429975adcc76dd557738ecc7c890ea18942eb321a5",
+    "zh:eb1f8368573d2370605d6dbf60f9aaa5b64e55741d96b5fb026dbfe91de67c0d",
+    "zh:fc1e12b713837b85daf6c3bb703d7795eaf1c5177aebae1afcf811dd7009f4b0",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,150 @@
+provider "aws" {
+  region = "eu-north-1"
+}  
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.31.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.31.32.0/19"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_nat_gateway" "gw_nat" {
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "gw_nat"
+  }
+
+      depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.31.96.0/19"
+  
+  tags = {
+    Name = "Private"
+  }  
+}    
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "172.31.32.0/19"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route" "r" {
+  route_table_id            = aws_route_table.r.id
+  destination_cidr_block    = "172.31.96.0/19"
+  nat_gateway_id = aws_nat_gateway.gw_nat.id
+}
+
+
+resource "aws_network_interface" "Ubuntu1" {
+  subnet_id   = aws_subnet.public.id
+  private_ips = ["172.16.32.7"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "Ubuntu1" {
+    ami = "ami-0767046d1677be5a0"
+    instance_type = "t2.micro" 
+    associate_public_ip_address = true
+
+    network_interface {
+    network_interface_id = aws_network_interface.Ubuntu1.id
+    device_index         = 0
+  }  
+}
+
+
+resource "aws_network_interface" "Ubuntu2" {
+  subnet_id   = aws_subnet.private.id
+  private_ips = ["172.16.96.7"]
+
+  tags = {
+    Name = "primary_network_interface1"
+  }
+}
+
+
+resource "aws_instance" "Ubuntu2" {
+    ami = "ami-0767046d1677be5a0"
+    instance_type = "t2.micro" 
+
+    network_interface {
+    network_interface_id = aws_network_interface.Ubuntu2.id
+    device_index         = 0
+  }  
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+}
+
+resource "tls_self_signed_cert" "example" {
+  key_algorithm   = "RSA"
+  private_key_pem = tls_private_key.example.private_key_pem
+
+  subject {
+    common_name  = "example.com"
+    organization = "ACME Examples, Inc"
+  }
+
+  validity_period_hours = 12
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key      = tls_private_key.example.private_key_pem
+  certificate_body = tls_self_signed_cert.example.cert_pem
+} 
+
+resource "aws_ec2_client_vpn_endpoint" "example" {
+  description            = "terraform-clientvpn-example"
+  server_certificate_arn = aws_acm_certificate.cert.arn
+  client_cidr_block      = "172.16.160.0/19"
+
+  authentication_options {
+    type                       = "certificate-authentication"
+    root_certificate_chain_arn = aws_acm_certificate.cert.arn
+  }
+
+  connection_log_options {
+    enabled               = true
+  }
+}
+
+  
+resource "aws_ec2_client_vpn_network_association" "example" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.example.id
+  subnet_id              = aws_subnet.private.id
+}
+
+resource "aws_ec2_client_vpn_authorization_rule" "example" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.example.id
+  target_network_cidr    = aws_subnet.private.cidr_block
+  authorize_all_groups   = true
+}
